c30b6b6eeeb7b23310296b54524deafe8adc99cb
itzzVarun <itsrealvarun@gmail.com>
2025-01-16 11:19:11 +0530

    Final Commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,38 +1,14 @@
-import React, { StrictMode, useState, useEffect } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
 import { BrowserRouter } from "react-router-dom";
 
-function Root() {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 4000); // Adjust time based on your hosting service delay
-
-    return () => clearTimeout(timer); // Cleanup on unmount
-  }, []);
-
-  if (isLoading) {
-    return (
-      <div style={{ textAlign: "center", marginTop: "20%" }}>
-        <h1>
-          This website is hosted on a free plan. Kindly wait sometime for
-          website to load.
-        </h1>
-      </div>
-    );
-  }
-
-  return <App />;
-}
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Root />
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
